fca2c9e76d24e66787ede4969610605f10b5f1ad
Hameesha Rantharu <45784711+hameesha@users.noreply.github.com>
2022-12-15 14:16:48 +0530

    new products

--- src/products.js
+++ src/products.js
@@ -6,6 +6,12 @@ import product5 from "./assets/products/5.png";
 import product6 from "./assets/products/6.webp";
 import product7 from "./assets/products/7.webp";
 import product8 from "./assets/products/8.webp";
+import product9 from "./assets/products/9.webp";
+import product10 from "./assets/products/10.webp";
+import product11 from "./assets/products/11.webp";
+import product12 from "./assets/products/12.webp";
+import product13 from "./assets/products/13.webp";
+import product14 from "./assets/products/14.webp";
 
 export const PRODUCTS = [
   {
@@ -55,5 +61,41 @@ export const PRODUCTS = [
     productName: "DON'T TRIP UNSTRUCTURED HAT",
     price: 40.0,
     productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Earbuds TWS bluetooth 6.0",
+    price: 50.0,
+    productImage: product9,
+  },
+  {
+    id: 10,
+    productName: "MaxPro Powerbank 10000mph",
+    price: 60.0,
+    productImage: product10,
+  },
+  {
+    id: 11,
+    productName: "Rolo v6 pro Smart Watch waterproof",
+    price: 10.0,
+    productImage: product11,
+  },
+  {
+    id: 12,
+    productName: "Smart Tv hub CELLTell",
+    price: 19.0,
+    productImage: product12,
+  },
+  {
+    id: 13,
+    productName: "Bluetooth Subwoofer with RGB lights",
+    price: 29.99,
+    productImage: product13,
+  },
+  {
+    id: 14,
+    productName: "ProLog Wireless mouse 3.0",
+    price: 9.99,
+    productImage: product14,
   },
 ];
